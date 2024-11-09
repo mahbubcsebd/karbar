2da940de2baf6f3c3853334d41731b546c3439dd
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2024-11-09 16:29:47 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ import whatsapp from '../app/assets/icons/whatsapp.svg';
 import ScrollToTop from '../app/components/ScrollToTop';
 import { ProductContext } from '../app/context/cartContext';
 import { cartReducer, initialState } from '../app/reducer/CartReducer';
+import { GoogleTagManager } from '@next/third-parties/google';
 import { usePathname } from 'next/navigation';
 import Script from 'next/script';
 import { ModalProvider } from '../app/reducer/ModalProvider';
@@ -33,31 +34,7 @@ const poppins = Poppins({
     subsets: ['latin'],
 });
 
-function GTMScript({ gtmId }) {
-    if (!gtmId) return null;
-
-    return (
-        <>
-            <Script id="google-tag-manager" strategy="afterInteractive">
-                {`
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','${gtmId}');
-                `}
-            </Script>
-            <noscript>
-                <iframe
-                    src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-                    height="0"
-                    width="0"
-                    style={{ display: 'none', visibility: 'hidden' }}
-                />
-            </noscript>
-        </>
-    );
-}
+ 
 
 export default function RootLayout({ children }) {
     const pathname = usePathname();
@@ -100,9 +77,11 @@ export default function RootLayout({ children }) {
         fetchData();
     }, []);
 
+    if (!gtmId) return null;
+
     return (
         <html lang="en">
-            {isGTMLoaded && <GTMScript gtmId={addManager?.tag_manager_id} />}
+           <GoogleTagManager gtmId={addManager?.tag_manager_id} />
             
             <body className={poppins.className}>
                 <Suspense fallback={<HomePreLoader />}>
