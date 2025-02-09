356236521d40d3137e105d2f53ba7b24efb77b5e
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2025-02-09 11:40:59 +0600

    performance optimization

--- app/components/HeroSlider.js
+++ app/components/HeroSlider.js
@@ -4,34 +4,15 @@ import { getHeroImage } from '@/utils/getHeroImage';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-
-import dynamic from 'next/dynamic'; // Lazy load Swiper
 import 'swiper/css';
 import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './hero.css';
 
-const Swiper = dynamic(() => import('swiper/react').then((mod) => mod.Swiper), {
-    ssr: false,
-});
-const SwiperSlide = dynamic(
-    () => import('swiper/react').then((mod) => mod.SwiperSlide),
-    { ssr: false }
-);
-const Autoplay = dynamic(
-    () => import('swiper/modules').then((mod) => mod.Autoplay),
-    { ssr: false }
-);
-const EffectFade = dynamic(
-    () => import('swiper/modules').then((mod) => mod.EffectFade),
-    { ssr: false }
-);
-const Navigation = dynamic(
-    () => import('swiper/modules').then((mod) => mod.Navigation),
-    { ssr: false }
-);
-const Pagination = dynamic(
-    () => import('swiper/modules').then((mod) => mod.Pagination),
-    { ssr: false }
-);
+import Head from 'next/head';
 
 const HeroSlider = () => {
     const [images, setImages] = useState([]);
@@ -43,7 +24,7 @@ const HeroSlider = () => {
                 const heroImages = await getHeroImage();
                 setImages(heroImages.data);
             } catch {
-                console.log('Hero slide image fetch failed');
+                console.log('hero slide image fetch failed');
             } finally {
                 setLoading(false);
             }
@@ -52,63 +33,75 @@ const HeroSlider = () => {
     }, []);
 
     return (
-        <div
-            id="hero"
-            className="hero"
-        >
-            <div className="hero-area">
-                <div className="hero-slider-container">
-                    {loading ? (
-                        <div className="w-full h-[300px] bg-gray-200 animate-pulse"></div>
-                    ) : (
-                        <Swiper
-                            spaceBetween={30}
-                            loop={true}
-                            autoplay={{
-                                delay: 2500,
-                                disableOnInteraction: false,
-                            }}
-                            effect={'fade'}
-                            navigation={true}
-                            pagination={{ clickable: true }}
-                            modules={[
-                                EffectFade,
-                                Navigation,
-                                Pagination,
-                                Autoplay,
-                            ]}
-                            className="mySwiper"
-                        >
-                            {images.map((img, index) => (
-                                <SwiperSlide key={img.id}>
-                                    <Link
-                                        href={img.url}
-                                        aria-label={`View details for ${img.title}`}
-                                    >
-                                        <div className="relative w-full h-[500px]">
+        <>
+            <Head>
+                {images?.map((img) => (
+                    <link
+                        key={img.id}
+                        rel="preload"
+                        href={img.image_url}
+                        as="image"
+                    />
+                ))}
+            </Head>
+            <div
+                id="hero"
+                className="hero"
+            >
+                <div className="hero-area">
+                    <div className="hero-slider-container">
+                        {loading ? (
+                            <div className="w-full h-[300px] bg-gray-200 animate-pulse"></div>
+                        ) : (
+                            <Swiper
+                                spaceBetween={30}
+                                loop={true}
+                                autoplay={{
+                                    delay: 2500,
+                                    disableOnInteraction: false,
+                                }}
+                                effect={'fade'}
+                                navigation={true}
+                                pagination={{
+                                    clickable: true,
+                                }}
+                                modules={[
+                                    EffectFade,
+                                    Navigation,
+                                    Pagination,
+                                    Autoplay,
+                                ]}
+                                className="mySwiper"
+                            >
+                                {images?.map((img, index) => (
+                                    <SwiperSlide key={img.id}>
+                                        <Link
+                                            href={img.url}
+                                            aria-label={`View details for ${img.title}`}
+                                        >
                                             <Image
                                                 src={img.image_url}
                                                 alt={img.title}
                                                 fill
                                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                                className="object-cover"
-                                                priority={index === 0} // Preload only first image
+                                                className="object-cover w-full h-auto"
+                                                priority={index === 0}
                                                 loading={
                                                     index === 0
                                                         ? 'eager'
                                                         : 'lazy'
                                                 }
-                                                quality={80}
+                                                quality={75}
                                             />
-                                        </div>
-                                    </Link>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                    )}
+                                        </Link>
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
